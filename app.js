1097d4ced559938eafa09112b39310fb8bd4e6b0
retro <roshnisingh87090@gmail.com>
2026-03-08 00:14:53 +0530

    added search functionality

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const openBtn = document.getElementById('addDocumentBtn')
-const dialog = document.getElementById('addDocumentDialog')
-const closeBtn = document.getElementById('closeModalBtn')
-const closeIocn = document.getElementById('closeIcon')
+const openBtn = document.getElementById("addDocumentBtn");
+const dialog = document.getElementById("addDocumentDialog");
+const closeBtn = document.getElementById("closeModalBtn");
+const closeIocn = document.getElementById("closeIcon");
 
-openBtn.addEventListener('click', () => {
-    dialog.showModal()
-})
+openBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
 
-closeBtn.addEventListener('click', () => {
-    dialog.close()
-})
+closeBtn.addEventListener("click", () => {
+    dialog.close();
+});
 
-closeIocn.addEventListener('click', () => {
-    dialog.close()
-})
+closeIocn.addEventListener("click", () => {
+    dialog.close();
+});
 
-function renderTable() {
-    const documents = JSON.parse(localStorage.getItem('documents'))
+function renderTable(documents) {
+    if (!documents) {
+        documents = JSON.parse(localStorage.getItem("documents"));
+    }
 
-    const table = document.getElementById('documentTable')
+    const tableBody = document.getElementById("document-table-body");
+    tableBody.innerHTML = "";
 
     documents.forEach((doc) => {
-        const tr = document.createElement('tr')
+        const tr = document.createElement("tr");
 
         tr.innerHTML = `
             <td><input type="checkbox" /></td>
@@ -32,16 +35,16 @@ function renderTable() {
                 <button class="btn-secondary">${getAction(doc.status)}</button>
                 <img src="./assets/3dot.svg" alt="3dots" />
             </td>
-        `
+        `;
 
-        table.appendChild(tr)
-    })
+        tableBody.appendChild(tr);
+    });
 }
 
 function getStatus(status) {
-    if (status === 'needs-signing') {
-        return `<span class="need-signing">Needs Signing</span>`
-    } else if (status === 'pending') {
+    if (status === "needs-signing") {
+        return `<span class="need-signing">Needs Signing</span>`;
+    } else if (status === "pending") {
         return `
             <div class="pending-primary">
                 <span class="pending">Pending</span>
@@ -49,21 +52,33 @@ function getStatus(status) {
                     <span style="color: #b5bdcd; font-style: italic">Waiting for</span>
                     <span style="color: #436d7c; font-style: italic">1 Person</span>
                 </p>
-            </div>`
-    } else if (status === 'completed') {
-        return `<span class="completed">Completed</span>`
+            </div>`;
+    } else if (status === "completed") {
+        return `<span class="completed">Completed</span>`;
     }
 }
 
 function getAction(status) {
-    if (status === 'needs-signing') return 'Sign now'
-    if (status === 'pending') return 'Preview'
-    if (status === 'completed') return 'Download PDF'
+    if (status === "needs-signing") return "Sign now";
+    if (status === "pending") return "Preview";
+    if (status === "completed") return "Download PDF";
 }
 
 function formatDateTime(dateTimeStr) {
-    const [date, time, meridiem] = dateTimeStr.split(' ')
-    return `<p>${date}</p><p>${time} ${meridiem}</p>`
+    const [date, time, meridiem] = dateTimeStr.split(" ");
+    return `<p>${date}</p><p>${time} ${meridiem}</p>`;
 }
 
-renderTable()
+const searchInput = document.getElementById("search-box");
+
+searchInput.addEventListener("input", (e) => {
+    const query = e.target.value.trim().toLowerCase();
+
+    const documents = JSON.parse(localStorage.getItem("documents"));
+
+    const filterDocuments = documents.filter((doc) => doc.name.toLowerCase().includes(query));
+
+    renderTable(filterDocuments);
+});
+
+renderTable();
